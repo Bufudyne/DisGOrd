e411cd2195355423bfbc770294ea9379f27ae256
Brozovic <arana.brozo@gmail.com>
2022-05-23 22:03:41 -0400

    Vite and Vue3 Template

---
+++ vue/vue-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="grid">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style lang="scss">
+@import "./assets/main.scss";
+
+#grid {
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 71px 240px auto 240px;
+  grid-template-rows: 46px auto 52px;
+  grid-template-areas:
+      "SL SN CI CI"
+      "SL CL CD UL"
+      "SL UI CD UL";
+}
+</style>
